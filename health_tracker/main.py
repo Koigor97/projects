333e9b9e2d457700b0ad64196338c49d591eec42
Koigor97 <turaysamuel101@yahoo.com>
2023-08-23 23:30:20 +0200

    Test functionality

--- health_tracker/main.py
+++ health_tracker/main.py
@@ -1,6 +1,11 @@
-from gui_config import window
+from gui_config import window, display_results
 from data_handling import PostData
 
 post_data = PostData()
+result = post_data.fetch_data()
+print(result)
+
+display_results(row_index=7, value=1, results=post_data.get_result())
+
 
 window.mainloop()
