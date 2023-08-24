2245c3a4261b80fb705ffca6383ca0281991c625
Koigor97 <turaysamuel101@yahoo.com>
2023-08-24 14:56:14 +0200

    Adding the rest of the widget

--- health_tracker/gui_config.py
+++ health_tracker/gui_config.py
@@ -1,4 +1,6 @@
+import tkinter
 from tkinter import *
+from tkinter import ttk
 
 ############################## CONFIGURATION CONSTANTS ##################################
 WIN_BG = "GhostWhite"
@@ -45,23 +47,23 @@ exercise_radio_btn.grid(row=1, column=2)
 
 # Text entry section
 text_field_frame = LabelFrame(event_frame, pady=10, bg=WIN_BG, borderwidth=0)
-text_field_frame.grid(row=2, column=0)
+text_field_frame.grid(row=4, column=0)
 text_field_label = Label(text_field_frame, text="Enter the activity: ", font=BODY_FONT, fg=TEXT_COLOR, bg=WIN_BG)
-text_field_label.grid(row=2, column=0, sticky="w")
+text_field_label.grid(row=4, column=0, sticky="w")
 
 text = Text(text_field_frame, width=30, height=5, font=BODY_FONT, fg=TEXT_COLOR, bg="gray95", padx=5, pady=5)
 text.insert(END, "type in the activity")
-text.grid(row=3, column=0, columnspan=2)
+text.grid(row=5, column=0, columnspan=2)
 
 # button section
 send_btn = Button(event_frame, text="Send", width=10, font=BODY_FONT, fg=TEXT_COLOR, highlightthickness=0)
-send_btn.grid(row=4, column=0)
+send_btn.grid(row=6, column=0)
 
 ####################################### OUTPUT SECTION##################################################
 results_label_frame = LabelFrame(event_frame, pady=15, bg=WIN_BG, borderwidth=0)
 results_label_frame.grid()
 results_label_text = Label(results_label_frame, text="Results", pady=8, bg=WIN_BG, fg=TEXT_COLOR, font=("Verdana", 12, "bold"))
-results_label_text.grid(row=5, column=1, columnspan=2)
+results_label_text.grid(row=7, column=1, columnspan=2)
 
 def display_results(row_index, value, results):
     header = ""
@@ -76,10 +78,10 @@ def display_results(row_index, value, results):
     _render_results_helper(results, the_row=row_index)
 
 
-def _render_results_helper(results, the_row = 6):
+def _render_results_helper(results, the_row = 8):
     the_font = HEADERS_FONT
     text_color = RESULTS_HEADER_COLOR
-    if not the_row == 6:
+    if not the_row == 8:
         the_font = BODY_FONT
         text_color = TEXT_COLOR
 
@@ -87,10 +89,43 @@ def _render_results_helper(results, the_row = 6):
         quantity_label = Label(results_label_frame, text=results[i], font=the_font, fg=text_color, bg=WIN_BG)
         quantity_label.grid(row=the_row, column=i)
 
+################################### SPINBOX & COMBOBOX #################################
+# The Spinbox section
+spinbox_label_frame = LabelFrame(event_frame, pady=5, bg=WIN_BG, borderwidth=0)
+spinbox_label_frame.grid(row=2, column=0)
+# labels= [, , ]
+# Age spinbox
+age_label = Label(spinbox_label_frame, text="Age:", font=BODY_FONT, fg=TEXT_COLOR, bg=WIN_BG)
+age_label.grid(row=2, column=0)
+
+age_value = StringVar(value=0)
+age_spinbox = tkinter.Spinbox(spinbox_label_frame, from_=16, to=110, textvariable=age_value, wrap=True,
+width=5, bg=WIN_BG, fg=TEXT_COLOR)
+age_spinbox.grid(row=2, column=1)
+print(age_spinbox.get())
+
+# weight spinbox
+weight_label = Label(spinbox_label_frame, text="Weight (kg):", font=BODY_FONT, fg=TEXT_COLOR, bg=WIN_BG)
+weight_label.grid(row=2, column=2)
+
+weight_value = StringVar(value=0)
+weight_spinbox = tkinter.Spinbox(spinbox_label_frame, from_=10, to=635, textvariable=weight_value, wrap=True,
+width=5, bg=WIN_BG, fg=TEXT_COLOR)
+weight_spinbox.grid(row=2, column=3)
+print(weight_spinbox.get())
+
+height_label = Label(spinbox_label_frame, text="Height (cm):", font=BODY_FONT, fg=TEXT_COLOR, bg=WIN_BG)
+height_label.grid(row=2, column=4)
+
+height_value = StringVar(value=0)
+height_spinbox = tkinter.Spinbox(spinbox_label_frame, from_=122, to=272, textvariable=height_value, wrap=True,
+width=5, bg=WIN_BG, fg=TEXT_COLOR)
+height_spinbox.grid(row=2, column=5)
+print(height_spinbox.get())
+
+###ComboBox section
+gender_label = Label(spinbox_label_frame, text="Gender:", font=BODY_FONT, fg=TEXT_COLOR, bg=WIN_BG)
+gender_label.grid(row=3, column=0)
 
 
-
-
-
-
-
+def get_all_values
