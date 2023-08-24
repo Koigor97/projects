40ff3a2033ca2c061f910a91dec6c8585f4478df
Koigor97 <turaysamuel101@yahoo.com>
2023-08-24 14:55:47 +0200

    Still testing functionality

--- health_tracker/main.py
+++ health_tracker/main.py
@@ -3,9 +3,9 @@ from data_handling import PostData
 
 post_data = PostData()
 result = post_data.fetch_data()
-print(result)
-
-display_results(row_index=7, value=1, results=post_data.get_result())
+# print(result)
+#
+# display_results(row_index=7, value=1, results=post_data.get_result())
 
 
 window.mainloop()
