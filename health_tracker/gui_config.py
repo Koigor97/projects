5b2b55b482f11d137d91fc17769e0a7fc74ef6bd
Koigor97 <turaysamuel101@yahoo.com>
2023-08-24 21:07:56 +0200

    modify the GUI interface

--- health_tracker/gui_config.py
+++ health_tracker/gui_config.py
@@ -13,7 +13,7 @@ RESULTS_HEADER_COLOR = "forest green"
 #################################### CREATING THE GUI ####################################
 window = Tk()
 window.title("Orion Fitness 🏋🏽‍♂️")
-window.geometry("800x800")
+window.geometry("800x1000")
 window.config(padx=20, pady=20, bg=WIN_BG)
 window.columnconfigure(index=1, weight=3)
 image = PhotoImage(file="fitness-logo.png")
@@ -37,11 +37,11 @@ choice_label_text = Label(choice_label_frame, text="Select an option: ",  font=B
 choice_label_text.grid(row=1, column=0)
 
 the_event_state = IntVar()
-nutrition_radio_btn = Radiobutton(choice_label_frame, text="Nutrition", value=1, variable=the_event_state, cursor="hand1",
+nutrition_radio_btn = Radiobutton(choice_label_frame, text="Nutrition", value=2, variable=the_event_state, cursor="hand1",
 bg=WIN_BG, highlightthickness=0, fg=TEXT_COLOR, font=BODY_FONT)
 nutrition_radio_btn.grid(row=1, column=1)
 
-exercise_radio_btn = Radiobutton(choice_label_frame, text="Exercise", value=2, variable=the_event_state, cursor="hand1",
+exercise_radio_btn = Radiobutton(choice_label_frame, text="Exercise", value=1, variable=the_event_state, cursor="hand1",
 bg=WIN_BG, highlightthickness=0, fg=TEXT_COLOR, font=BODY_FONT)
 exercise_radio_btn.grid(row=1, column=2)
 
@@ -52,7 +52,7 @@ text_field_label = Label(text_field_frame, text="Enter the activity: ", font=BOD
 text_field_label.grid(row=4, column=0, sticky="w")
 
 text = Text(text_field_frame, width=30, height=5, font=BODY_FONT, fg=TEXT_COLOR, bg="gray95", padx=5, pady=5)
-text.insert(END, "type in the activity")
+text.insert(1.0, "type in the activity")
 text.grid(row=5, column=0, columnspan=2)
 
 # button section
@@ -81,7 +81,7 @@ def display_results(row_index, value, results):
 def _render_results_helper(results, the_row = 8):
     the_font = HEADERS_FONT
     text_color = RESULTS_HEADER_COLOR
-    if not the_row == 8:
+    if not the_row == 9:
         the_font = BODY_FONT
         text_color = TEXT_COLOR
 
@@ -102,6 +102,7 @@ age_value = StringVar(value=0)
 age_spinbox = tkinter.Spinbox(spinbox_label_frame, from_=16, to=110, textvariable=age_value, wrap=True,
 width=5, bg=WIN_BG, fg=TEXT_COLOR)
 age_spinbox.grid(row=2, column=1)
+age_spinbox.grid_configure(padx=5)
 
 # weight spinbox
 weight_label = Label(spinbox_label_frame, text="Weight (kg):", font=BODY_FONT, fg=TEXT_COLOR, bg=WIN_BG)
@@ -111,6 +112,7 @@ weight_value = StringVar(value=0)
 weight_spinbox = tkinter.Spinbox(spinbox_label_frame, from_=10, to=635, textvariable=weight_value, wrap=True,
 width=5, bg=WIN_BG, fg=TEXT_COLOR)
 weight_spinbox.grid(row=2, column=3)
+weight_spinbox.grid_configure(padx=5)
 
 height_label = Label(spinbox_label_frame, text="Height (cm):", font=BODY_FONT, fg=TEXT_COLOR, bg=WIN_BG)
 height_label.grid(row=2, column=4)
@@ -119,6 +121,7 @@ height_value = StringVar(value=0)
 height_spinbox = tkinter.Spinbox(spinbox_label_frame, from_=122, to=272, textvariable=height_value, wrap=True,
 width=5, bg=WIN_BG, fg=TEXT_COLOR)
 height_spinbox.grid(row=2, column=5)
+height_spinbox.grid_configure(padx=5)
 
 ###ComboBox section
 gender_frame = LabelFrame(event_frame, bg=WIN_BG, borderwidth=0)
@@ -129,6 +132,21 @@ gender_label.grid(row=3, column=0)
 
 gender_combobox = ttk.Combobox(gender_frame, values=["Male", "Female", "Others"])
 gender_combobox.grid(row=3, column=1, columnspan=2)
+gender_combobox.grid_configure(padx=10)
 
 for widget in event_frame.winfo_children():
     widget.grid_configure(padx=20, pady=10)
+
+# Save buttons section
+save_result_frame = LabelFrame(bg=WIN_BG, padx=10, pady=5, borderwidth=0)
+save_result_frame.grid(row=10, column=1)
+
+save_local_btn = Button(save_result_frame, text="Save In Local 📥", font=BODY_FONT, fg=TEXT_COLOR,
+highlightthickness=0)
+save_local_btn.grid(row=10, column=0)
+save_local_btn.grid_configure(padx=6, pady=15)
+
+save_cloud_btn = Button(save_result_frame, text="Save In Cloud ☁️", font=BODY_FONT, fg=TEXT_COLOR,
+highlightthickness=0)
+save_cloud_btn.grid(row=10, column=2)
+save_cloud_btn.grid_configure(padx=6, pady=15)
