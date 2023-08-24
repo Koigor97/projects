82f8651b4918e6f7c547f53c644b78d48b327aa5
Koigor97 <turaysamuel101@yahoo.com>
2023-08-24 21:09:26 +0200

    Add some encapsulation and logic

--- health_tracker/data_handling.py
+++ health_tracker/data_handling.py
@@ -1,13 +1,18 @@
 import requests
+import pandas as pd
+
 
 class PostData:
     def __init__(self):
-        self.__app_id = "YOUR_API_ID"
-        self.__api_key = "YOUR_API_KEY"
+        self.__app_id = "xxxxxxxxxxxxxxx"
+        self.__api_key = "xxxxxxxxxxxxxxxxxxxxxxxx"
         self.__exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
         self.__nutrition_endpoint = "https://trackapi.nutritionix.com/v2/natural/nutrients"
-        self.__entry = "USER_ENTRY"
-
+        self.__entry = ""
+        self.__gender = ""
+        self.__age = ""
+        self.__weight = ""
+        self.__height = ""
 
     def __get_header(self):
         header = {
@@ -19,11 +24,11 @@ class PostData:
 
     def __get_body(self):
         body = {
-            "query": self.__entry,
-            "gender": "female",
-            "weight_kg": 72.5,
-            "height_cm": 167.64,
-            "age": 30
+            "query": self.__get_content(),
+            "gender": self.__get_gender(),
+            "weight_kg": self.__get_weight(),
+            "height_cm": self.__get_height(),
+            "age": self.__get_age()
         }
         return body
 
@@ -45,3 +50,31 @@ class PostData:
         result = self.fetch_data()["exercises"][0]
         the_result = [result["name"].title(), result["met"], result["duration_min"], result["nf_calories"]]
         return the_result
+
+
+    def set_body(self, content, gender, weight, height, age):
+        self.__entry = content
+        self.__gender = gender
+        self.__age = age
+        self.__weight = weight
+        self.__height = height
+
+
+    def __get_content(self):
+        return self.__entry
+
+
+    def __get_gender(self):
+        return self.__gender.lower()
+
+
+    def __get_age(self):
+        return int(self.__age)
+
+
+    def __get_weight(self):
+        return float(self.__weight)
+
+
+    def __get_height(self):
+        return float(self.__height)
