15d190a33d98693838eac60641448e4651aaff7b
Koigor97 <turaysamuel101@yahoo.com>
2023-08-25 12:18:07 +0200

    Complete the Orion Health Racker app

--- health_tracker/main.py
+++ health_tracker/main.py
@@ -1,16 +1,26 @@
 from gui_config import *
+from tkinter import messagebox
 from data_handling import PostData, SaveLocally
 # import requests
 
-content = ""
 post_data = PostData()
 local_storage = SaveLocally()
 
 def send_btn_helper():
-    post_data.set_body(content=text.get(1.0, END), gender=gender_combobox.get(), age=age_spinbox.get(),
-                       weight=weight_spinbox.get(), height=height_spinbox.get())
-    local_storage.set_result(post_data.fetch_data())
-    display_results(row_index=9, value=1, results=post_data.get_result())
+    if is_entry_fields_empty(content=text.get(1.0, END), gender=gender_combobox.get(), age=age_spinbox.get(),
+weight=weight_spinbox.get(), height=height_spinbox.get()):
+        post_data.set_body(content=text.get(1.0, END), gender=gender_combobox.get(), age=age_spinbox.get(),
+                           weight=weight_spinbox.get(), height=height_spinbox.get())
+        local_storage.set_result(post_data.fetch_data())
+        display_results(row_index=9, value=1, results=post_data.get_result())
+
+
+def is_entry_fields_empty(content, gender, age, weight, height):
+    if (content == "") or (gender == "") or (age == "") or (weight == "") or (height == ""):
+        messagebox.showinfo(title="Empty field", message="You have an empty input field. Please enter the data needed 🙂")
+    else:
+        return True
+
 
 send_btn.config(command=send_btn_helper)
 save_local_btn.config(command=local_storage.save_data)
