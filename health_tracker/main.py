f19fc467d8f401b04d2384e77a27b93702534580
Koigor97 <turaysamuel101@yahoo.com>
2023-08-25 09:37:13 +0200

    Server Error persists. Import SaveLocally class

--- health_tracker/main.py
+++ health_tracker/main.py
@@ -1,8 +1,9 @@
 from gui_config import *
-from data_handling import PostData
-import requests
+from data_handling import PostData, SaveLocally
+# import requests
 
 post_data = PostData()
+local_storage = SaveLocally()
 
 def send_btn_helper():
     post_data.set_body(content=text.get(1.0, END), gender=gender_combobox.get(), age=age_spinbox.get(),
@@ -14,19 +15,20 @@ def send_btn_helper():
 send_btn.config(command=send_btn_helper)
 # window.mainloop()
 
-sheet_endpoint = "https://api.sheety.co/19f150e92e462797dab865579a69960e/workoutTracker/workouts"
-header = {
-    "Content-Type": "application/json"
-}
-body = {
-    "workout": {
-       "date": "23/09/2023",
-        "time": "10:30:00",
-        "exercise": "Running",
-        "duration": 40,
-        "calories": 240
-    }
-}
-
-response = requests.post(url=sheet_endpoint, json=body, headers=header)
-response.raise_for_status()
+# TESTING TO LOG THE DATA TO GOOGLE SHEET [test was a fail - received a 500 Server Error]
+# sheet_endpoint = "https://api.sheety.co/19f150e92e462797dab865579a69960e/workoutTracker/workouts"
+# header = {
+#     "Content-Type": "application/json"
+# }
+# body = {
+#     "workout": {
+#        "date": "23/09/2023",
+#         "time": "10:30:00",
+#         "exercise": "Running",
+#         "duration": 40,
+#         "calories": 240
+#     }
+# }
+#
+# response = requests.post(url=sheet_endpoint, json=body, headers=header)
+# response.raise_for_status()
