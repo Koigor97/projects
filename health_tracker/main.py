05a8a668729fad0c5e87e8c0ed2be6cd1418df22
Koigor97 <turaysamuel101@yahoo.com>
2023-08-25 10:41:42 +0200

    Testing to retrieve data fro local storage [test passed]

--- health_tracker/main.py
+++ health_tracker/main.py
@@ -2,18 +2,19 @@ from gui_config import *
 from data_handling import PostData, SaveLocally
 # import requests
 
+content = ""
 post_data = PostData()
 local_storage = SaveLocally()
 
 def send_btn_helper():
     post_data.set_body(content=text.get(1.0, END), gender=gender_combobox.get(), age=age_spinbox.get(),
                        weight=weight_spinbox.get(), height=height_spinbox.get())
-    result = post_data.fetch_data()
-    print(result)
-    display_results(row_index=8, value=1, results=post_data.get_result())
+    local_storage.set_result(post_data.fetch_data())
+    display_results(row_index=9, value=1, results=post_data.get_result())
+    local_storage.save_data()
 
 send_btn.config(command=send_btn_helper)
-# window.mainloop()
+window.mainloop()
 
 # TESTING TO LOG THE DATA TO GOOGLE SHEET [test was a fail - received a 500 Server Error]
 # sheet_endpoint = "https://api.sheety.co/19f150e92e462797dab865579a69960e/workoutTracker/workouts"
