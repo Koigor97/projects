7552de45443013291c978f8e068d7fe58e09cffa
Koigor97 <turaysamuel101@yahoo.com>
2023-12-09 14:40:23 +0200

    add task date, description, select field components

---
+++ dueDate/due-date/src/components/createAssignmentTask/taskDateField.tsx
@@ -0,0 +1,34 @@
+import React, { FC, ReactElement } from 'react';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers';
+import { IDateField } from './interfaces/IDateField';
+import PropTypes from 'prop-types';
+
+export const TaskDateField: FC<IDateField> = (props): ReactElement => {
+  //   Destructure
+  const {
+    value = new Date(),
+    disabled = false,
+    onChange = (date) => console.log(date),
+  } = props;
+
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DatePicker
+          label="Task Date"
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+        />
+      </LocalizationProvider>
+    </>
+  );
+};
+
+TaskDateField.propTypes = {
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  value: PropTypes.instanceOf(Date),
+};
--- dueDate/due-date/src/components/createAssignmentTask/taskDescriptionField.tsx
+++ dueDate/due-date/src/components/createAssignmentTask/taskDescriptionField.tsx
@@ -1,7 +1,7 @@
 import React, { FC, ReactElement } from 'react';
 import { TextField } from '@mui/material';
-
 import { ITextField } from './interfaces/ITextField';
+import PropTypes from 'prop-types';
 
 export const TaskDescriptionField: FC<ITextField> = (props): ReactElement => {
   const { onChange = (e) => console.log(e), disabled = false } = props;
@@ -21,4 +21,9 @@ export const TaskDescriptionField: FC<ITextField> = (props): ReactElement => {
       disabled={disabled}
     />
   );
+};
+
+TaskDescriptionField.propTypes = {
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
 };
