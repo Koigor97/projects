d943467a1eb1f4a829d7c10c2ef1a429ee1c707b
Koigor97 <turaysamuel101@yahoo.com>
2023-12-09 14:37:13 +0200

    helper functions

---
+++ dueDate/due-date/src/components/taskCounter/helperFunctions/correctBorderColor.ts
@@ -0,0 +1,13 @@
+import { TaskCounterStatusType } from '../interfaces/ITaskCounter';
+import { Status } from '../../createAssignmentTask/enums/Status';
+
+export const correctBorderColors = (status: TaskCounterStatusType): string => {
+  switch (status) {
+    case Status.todo:
+      return 'error.light';
+    case Status.inProgress:
+      return 'warning.light';
+    case Status.completed:
+      return 'success.light';
+  }
+};
