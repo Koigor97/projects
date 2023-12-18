f2c8fa607d6713ba84533b072214805c26782a63
Koigor97 <turaysamuel101@yahoo.com>
2023-12-18 09:40:56 +0200

    create the User schema

---
+++ housepartyhub/api/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// creating a Schema - setting the rules and needed required data
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+// exporting the User model
+module.exports = User;
