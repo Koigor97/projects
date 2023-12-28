4ef53c4d92a6cc5ff7a3a7ec1f399d495e613e9e
Koigor97 <turaysamuel101@yahoo.com>
2023-12-28 11:12:12 +0200

    made changes to startScreen component

--- outsmart-me/src/components/StartScreen.js
+++ outsmart-me/src/components/StartScreen.js
@@ -1,7 +1,8 @@
 function StartScreen({ numOfQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to the React Quiz</h2>
+      <h2>Try and OutSmart me.</h2>
+      <h3> That is if you can 🤭...</h3>
       <h4>{numOfQuestions} question to test your React mastery</h4>
       <button
         className="btn btn-ui"
