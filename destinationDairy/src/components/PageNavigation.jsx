ef59cee20e5b0fc0433c298014753102e8c12e25
Koigor97 <turaysamuel101@yahoo.com>
2023-12-29 12:00:47 +0200

    modify pageNav and pageNav css module

--- destinationDairy/src/components/PageNavigation.jsx
+++ destinationDairy/src/components/PageNavigation.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 import styles from "./PageNav.module.css";
 
 function PageNavigation() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -13,6 +16,11 @@ function PageNavigation() {
         </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
