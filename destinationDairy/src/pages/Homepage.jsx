9125aaea4d6b2e1d8d51c401eb31ce6865e02061
Koigor97 <turaysamuel101@yahoo.com>
2023-12-29 12:01:33 +0200

    add data to homepage and homepage css modules

--- destinationDairy/src/pages/Homepage.jsx
+++ destinationDairy/src/pages/Homepage.jsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
 import PageNavigation from "../components/PageNavigation";
+import styles from "./Homepage.module.css";
 
-function Homepage() {
+export default function Homepage() {
   return (
-    <div>
+    <main className={styles.homepage}>
       <PageNavigation />
-      <h1>DestionDairy</h1>
-      <Link to="/app">Go to App</Link>
-    </div>
+      <section>
+        <h1>
+          DestinationDiary.
+          <br />
+          Your personalized travel memoir.
+        </h1>
+        <h2>
+          Capture every step of your journey with our interactive map, where
+          each pin represents a cherished adventure. Pen down vivid diary
+          entries. Join a vibrant community of explorers, where your wanderlust
+          stories find a home.
+        </h2>
+        <Link to="/app" className="cta">
+          Start journaling
+        </Link>
+      </section>
+    </main>
   );
 }
-
-export default Homepage;
