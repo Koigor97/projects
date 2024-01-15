e243f2184762295642e10188e54775c72c4ade2a
Koigor97 <turaysamuel101@yahoo.com>
2024-01-15 22:10:38 +0200

    refactor City.jsx

--- destinationDairy/src/components/City.jsx
+++ destinationDairy/src/components/City.jsx
@@ -1,5 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { useParams, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+
+import { useCities } from "../contexts/CitiesContext";
+// import ButtonBack from "./ButtonBack";
+
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -11,63 +16,49 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  const x = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const id = useParams();
+  const { getCityById, currentCity } = useCities();
 
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  useEffect(() => {
+    getCityById(id);
+  }, [id]);
 
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
-    <>
-      <h1>City {x.id}</h1>
-      <p>
-        Position: {lat}, {lng}
-      </p>
-    </>
-    // <div className={styles.city}>
-    //   <div className={styles.row}>
-    //     <h6>City name</h6>
-    //     <h3>
-    //       <span>{emoji}</span> {cityName}
-    //     </h3>
-    //   </div>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>{cityName}</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
 
-    //   <div className={styles.row}>
-    //     <h6>You went to {cityName} on</h6>
-    //     <p>{formatDate(date || null)}</p>
-    //   </div>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on:</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
 
-    //   {notes && (
-    //     <div className={styles.row}>
-    //       <h6>Your notes</h6>
-    //       <p>{notes}</p>
-    //     </div>
-    //   )}
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
 
-    //   <div className={styles.row}>
-    //     <h6>Learn more</h6>
-    //     <a
-    //       href={`https://en.wikipedia.org/wiki/${cityName}`}
-    //       target="_blank"
-    //       rel="noreferrer"
-    //     >
-    //       Check out {cityName} on Wikipedia &rarr;
-    //     </a>
-    //   </div>
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
 
-    //   <div>
-    //     <ButtonBack />
-    //   </div>
-    // </div>
+      <div>{/* <ButtonBack /> */}</div>
+    </div>
   );
 }
 
