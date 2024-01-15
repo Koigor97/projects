3fa92a604bb8b230bcb1fb02833048e113ab4df4
Koigor97 <turaysamuel101@yahoo.com>
2024-01-15 22:10:05 +0200

    add currentCity useState hook to CitiesContext.jsx

--- destinationDairy/src/contexts/CitiesContext.jsx
+++ destinationDairy/src/contexts/CitiesContext.jsx
@@ -6,6 +6,8 @@ const CitiesContext = createContext();
 export function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
   const URL = `http://localhost:8000`;
 
   // loading the cities data from the data file
@@ -27,8 +29,23 @@ export function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCityById(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert(": There was an error loading the data");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider
+      value={{ cities, isLoading, currentCity, getCityById }}
+    >
       {children}
     </CitiesContext.Provider>
   );
